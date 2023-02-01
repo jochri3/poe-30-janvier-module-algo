cde0aee18350974704cde1952f30e8b5bf4b53ad
Christian Lisangola <jochri3@gmail.com>
2023-02-01 12:01:07 +0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+// import promptSync from 'prompt-sync'
+
+// const prompt = promptSync()
+
+// const a = parseFloat(prompt('Nombre 1: '))
+// const b = parseFloat(prompt('Nombre 2: '))
+// console.log(`${a} + ${b} = ${a + b}`)
